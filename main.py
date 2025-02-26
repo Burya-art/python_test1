9bd419276fab74991f24fc9ed191f366598a7c95
Ihor Burchik <burchik555@gmail.com>
2025-02-26 15:42:06 +0200

    Практика
    1

--- main.py
+++ main.py
@@ -1,33 +1,50 @@
-# Напиши класс, что принимает при инициализации user_email и сохраняет в атрибут класса.
-# У него также есть метод send_email, который принимает message, берет user_email из
-# атрибутов класса и принтит что-то типа 'Емейл отправлен на тут_емейл_юзера: тут_текст_из_message'
-# Напиши дальше инициализацию с передачей емейла юзера, а потом вызовом send_email.
+class Image:
+    def __init__(self, resolution, title, extension):
+        self.resolution = resolution
+        self.title = title
+        self.extension = extension
 
-class Post:
-    def __init__(self, content):
-        self.content = content
-        self.votes = 0  # Начальное количество голосов
-
-    def upvote(self):
-        self.votes += 1  # Увеличиваем количество голосов на 1
-
-    def downvote(self):
-        self.votes -= 1  # Уменьшаем количество голосов на 1
-
-    def get_votes(self):
-        return self.votes
-
-# Пример использования
-post = Post("Это пример поста.")
-post.upvote()  # Голосование за пост
-post.upvote()  # Еще одно голосование
-post.upvote()  # Еще одно голосование
-post.upvote()
-post.upvote()  
-post.upvote()
+    def resize(self, new_resolution, new_title):
+        self.resolution = new_resolution
+        self.title = new_title
 
 
-post.downvote()  # Голосование против поста
-post.downvote()  # Голосование против поста
-post.downvote()  # Голосование против поста
-print(post.get_votes())  # Выведет: 1
+img = Image('1920x1080', "Sunset", "jpg")
+print(img.resolution, img.title, img.extension)
+
+img.resize('860x540', 'Night')
+print(img.resolution)
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Post:
+#     def __init__(self, content):
+#         self.content = content
+#         self.votes = 0  # Начальное количество голосов
+#
+#     def upvote(self, qty):
+#         self.votes += qty
+#
+#     def downvote(self):
+#         self.votes -= 1  # Уменьшаем количество голосов на 1
+#
+#     def get_votes(self):
+#         return self.votes
+#
+# # Пример использования
+# post = Post("Это пример поста.")
+# post.upvote(5)  # Голосование за пост
+#
+#
+# post.downvote()  # Голосование против поста
+# print(post.get_votes())  # Выведет: 1
