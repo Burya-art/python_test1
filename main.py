85e794c5cbb9bbe3b79018b0699c32126ccc5bbb
Ihor Burchik <burchik555@gmail.com>
2025-03-11 22:46:48 +0200

    Обновил код в main.py

--- main.py
+++ main.py
@@ -1,39 +1,16 @@
-with open('passwords.txt', 'r', encoding='utf-8') as file:
-    passwords = [line.strip() for line in file]
+class BankAccount:
+    def __init__(self, name, balance):
+        self.name = name
+        self.balance = balance
 
-used_passwords = []
-
-class User:
-
-    def __init__(self, login, password):
-        self.login = login
-        self.__password = password
-
-    @property
-    def password(self):
-        return self.__password
-
-    @password.setter
-    def password(self, value):
-        if not isinstance(value, str):
-            raise TypeError("Пароль должен быть строкой")
-        if len(value) < 4:
-            raise ValueError("Длина пароля слишком мала, минимум должно быть 4 символа")
-        if len(value) > 12:
-            raise ValueError("Длина пароля слишком велика, максимум должно быть 12 символов")
-        try:
-            if value in passwords:
-                raise ValueError("Это слишком лёгкий пароль")
-        except ValueError as e:
-            print(e)
-
-        self.__password = value
-        used_passwords.append(value)
+    def __add__(self, other):
+        print('__add__ call')
+        if isinstance(other, (int, float)):
+            return self.name - other
 
 
-r = User('aaa', 123)
+account = BankAccount(50, 100)
 
-new_password = input("Введите пароль: ")
+new = account + 500
 
-r.password = new_password
-print(used_passwords)
+print(new)
